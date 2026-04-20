cb9819bab3fe912780f0403b3437ad9064df8da1
templechan <templechan@126.com>
2026-04-20 12:42:40 +0800

    部署改为增量更新，提高部署速度

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,13 +48,13 @@ export const metadata: Metadata = {
         siteName: "T.AI - AI助手",
         locale: "zh_CN",
         type: "website",
-        images: "https://tai.templechann.com/assets/images/logo.png",
+        images: [{ url: "https://tai.templechann.com/assets/images/logo.png" }],
     },
     twitter: {
         card: "summary_large_image",
         title: "T.AI - AI助手",
         description: "Chat with T.AI – your intelligent assistant for coding, content creation, file reading, and more. Upload documents, engage in long-context conversations, and get expert help in AI, natural language processing, and beyond. | T.AI 助力编程代码开发、创意写作、文件处理等任务，支持文件上传及长文本对话，随时为您提供高效的AI支持。",
-        images: "https://tai.templechann.com/assets/images/logo.png",
+        images: [{ url: "https://tai.templechann.com/assets/images/logo.png" }],
     },
     robots: {
         index: true,
